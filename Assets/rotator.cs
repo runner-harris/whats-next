4d68a25764f1b823930d9da839acd6226f9eedd6
mccormacktc <89530467+mccormacktc@users.noreply.github.com>
2022-11-09 12:23:19 -0500

    rotate added

--- Assets/rotator.cs
+++ Assets/rotator.cs
@@ -6,8 +6,8 @@ public class rotator : MonoBehaviour
 {
 
     // Update is called once per frame
-    void Update()
+    void rotate()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
     }
 }
